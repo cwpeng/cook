37defbdb95bf7365a0b8dd2d3875097805af9e31
cwpeng <padalab@gmail.com>
2015-02-26 12:15:30 +0800

    Appengine and Android Environment

---
+++ src/pada/data/Cookbook.java
@@ -0,0 +1,8 @@
+public class Cookbook{
+	public long id;
+	public String name;
+	public Cookbook(long id, String name){
+		this.id=id;
+		this.name=name;
+	}
+}
---
+++ src/pada/data/Material.java
@@ -0,0 +1,8 @@
+public class Material{
+	public long id;
+	public String name;
+	public Material(long id, String name){
+		this.id=id;
+		this.name=name;
+	}
+}
